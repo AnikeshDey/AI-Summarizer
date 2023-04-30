1469dce2d037c656c7fbc8d383f8f56e244319bd
AnikeshDey <anikeshdey2000@gmail.com>
2023-04-30 13:16:14 +0530

    first commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+
+const Demo = () => {
+    return (
+        <div>
+            Demo
+        </div>
+    )
+}
+
+export default Demo;
